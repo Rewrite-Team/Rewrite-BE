fe82f77550c7bd239cca37424d38224bc126b6ad
yong203 <jk07028@naver.com>
2026-09-04 16:33:14 +0900

    :memo: docs: LlmJob 프로젝션 조회 설명 추가

--- src/main/java/com/daon/rewrite/llmjob/repository/LlmJobRepository.java
+++ src/main/java/com/daon/rewrite/llmjob/repository/LlmJobRepository.java
@@ -17,12 +17,16 @@ import java.util.Optional;
 
 public interface LlmJobRepository extends JpaRepository<LlmJob, String> {
 
+    // 조회 결과 중 필요한 두 필드만 받을 수 있도록 만든 인터페이스 기반 프로젝션
+    // Spring Data JPA가 조회결과의 alias를 getter 이름과 연결해주기에 별도의 구현 클래스를 작성하지 않아도 Spring Data 가 런타임에 JobTarget 구현 객체를 만들어 준다.
     interface JobTarget {
         String getTargetId();
 
         LlmJobTargetType getTargetType();
     }
 
+    // LlmJob 전체 엔티티를 조회하지 않고 특정 작업의 targetId와 targetType만 가져오며, alias를 지정하여 프로젝션과 연결
+    // 엔티티 전체가 아닌 필요한 필드만 가져오기에 가볍다
     @Query("select job.targetId as targetId, job.targetType as targetType from LlmJob job where job.id = :id")
     Optional<JobTarget> findTargetById(@Param("id") String id);
 
